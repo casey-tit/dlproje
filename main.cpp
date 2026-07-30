de12895ceba9fc70cd431a442dd7e8bc0d21d73f
casey-tit <caseysmhtitanyum@gmail.com>
2026-07-31 01:13:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <windows.h>
 #include <GL/gl.h>
 
-// OpenGL hook ve ortam karartma mantığı iskeleti
+// OpenGL orijinal fonksiyon adresleri için pointerlar
+typedef void (APIENTRY* fn_glMaterialfv)(GLenum face, GLenum pname, const GLfloat* params);
+fn_glMaterialfv o_glMaterialfv = nullptr;
+
+// Oyuncular parıl parıl parlasın diye renk tanımı (Neon Beyaz/Parlak)
+void APIENTRY hooked_glMaterialfv(GLenum face, GLenum pname, const GLfloat* params) {
+    if (pname == GL_AMBIENT_DIFFUSE || pname == GL_EMISSION) {
+        GLfloat brightColor[] = { 2.0f, 2.0f, 2.0f, 1.0f }; // Ekstra parlaklık efekti
+        o_glMaterialfv(face, pname, brightColor);
+        return;
+    }
+    o_glMaterialfv(face, pname, params);
+}
+
+DWORD WINAPI MainThread(LPVOID lpParam) {
+    // Burada ileride fonksiyon hook (MinHook vb.) entegrasyonu yapılacak
+    while (true) {
+        Sleep(100);
+    }
+    return 0;
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
-        // DLL oyuna enjekte olduğunda çalışacak kısım
-        MessageBoxA(NULL, "DLL Basariyla Enjekte Edildi!", "Bilgi", MB_OK);
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, nullptr, 0, nullptr);
         break;
     case DLL_PROCESS_DETACH:
         break;
