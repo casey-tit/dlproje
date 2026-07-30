037ff5dbc29364f8f960ffbc870c0a867eb13864
casey-tit <caseysmhtitanyum@gmail.com>
2026-07-31 01:15:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include <windows.h>
 #include <GL/gl.h>
 
-// OpenGL orijinal fonksiyon adresleri için pointerlar
-typedef void (APIENTRY* fn_glMaterialfv)(GLenum face, GLenum pname, const GLfloat* params);
-fn_glMaterialfv o_glMaterialfv = nullptr;
-
-// Oyuncular parıl parıl parlasın diye renk tanımı (Neon Beyaz/Parlak)
-void APIENTRY hooked_glMaterialfv(GLenum face, GLenum pname, const GLfloat* params) {
-    if (pname == GL_AMBIENT_DIFFUSE || pname == GL_EMISSION) {
-        GLfloat brightColor[] = { 2.0f, 2.0f, 2.0f, 1.0f }; // Ekstra parlaklık efekti
-        o_glMaterialfv(face, pname, brightColor);
-        return;
-    }
-    o_glMaterialfv(face, pname, params);
-}
-
 DWORD WINAPI MainThread(LPVOID lpParam) {
-    // Burada ileride fonksiyon hook (MinHook vb.) entegrasyonu yapılacak
     while (true) {
         Sleep(100);
     }
